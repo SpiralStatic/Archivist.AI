fcd0dbcb72888060990f263d2d6fc10239f7bb57
Hannah Tsoi <hannah.tsoi+work@protonmail.com>
2023-09-01 16:08:58 +0100

    Added ChatServiceTests

---
+++ Archivist.AI.Core.Tests/ChatServiceTests.cs
@@ -0,0 +1,59 @@
+using NSubstitute;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels.ResponseModels;
+using OpenAI.ObjectModels.SharedModels;
+using Shouldly;
+using static OpenAI.ObjectModels.StaticValues;
+
+namespace Archivist.AI.Core.Tests;
+
+public class ChatServiceTests
+{
+    private ChatService _sut;
+    private readonly IOpenAIService _openAIService = Substitute.For<IOpenAIService>();
+    private readonly IEmbeddingsService _embeddingsService = Substitute.For<IEmbeddingsService>();
+
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new ChatService(_embeddingsService, _openAIService);
+    }
+
+    #region GetChatResponse
+    [Test]
+    public async Task GetChatResponse_GivenChatCompletionApiBadResponse_ThrowsArchivistException()
+    {
+        _embeddingsService.GetRelatedEmbeddings(default!)
+            .ReturnsForAnyArgs(new List<Embedding>());
+
+        _openAIService.ChatCompletion.CreateCompletion(default!)
+            .ReturnsForAnyArgs(new ChatCompletionCreateResponse { Error = new Error() });
+
+        const string text = "test";
+
+        await _sut.GetChatResponse(text).ShouldThrowAsync<ArchivistException>();
+    }
+
+    [Test]
+    public async Task GetChatResponse_GivenChatCompletionApiSuccessfulResponse_Returns()
+    {
+        _embeddingsService.GetRelatedEmbeddings(default!)
+            .ReturnsForAnyArgs(new List<Embedding>());
+
+        _openAIService.ChatCompletion.CreateCompletion(default!)
+            .ReturnsForAnyArgs(new ChatCompletionCreateResponse
+            {
+                Choices = new List<ChatChoiceResponse>
+                {
+                    new ChatChoiceResponse { Message = new ChatMessage(ChatMessageRoles.Assistant, "hello there") }
+                }
+            });
+
+        const string text = "test";
+
+        var result = await _sut.GetChatResponse(text);
+        result.ShouldBe("hello there");
+    }
+    #endregion
+}
